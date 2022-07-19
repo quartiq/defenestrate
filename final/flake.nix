046dd6a63f9956786f9f6d0ce484009e54725f52
Sebastien Bourdeauducq <sb@m-labs.hk>
2022-07-19 19:04:12 +0800

    release-7

--- final/flake.nix
+++ final/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.artiq.url = git+https://github.com/m-labs/artiq.git;
+  inputs.artiq.url = git+https://github.com/m-labs/artiq.git?ref=release-7;
   outputs = { self, artiq }: {
     nixosConfigurations.artiq = artiq.inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
