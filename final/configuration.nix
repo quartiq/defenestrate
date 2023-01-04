10d529da0a5b1d82fd8f93544f565f7bdeacde51
Sebastien Bourdeauducq <sb@m-labs.hk>
2023-01-04 11:30:42 +0800

    do not use specialArgs. Closes #1

--- final/configuration.nix
+++ final/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, artiq, ... }:
+{ artiq }: { config, pkgs, ... }:
 
 {
   imports =
--- final/flake.nix
+++ final/flake.nix
@@ -3,8 +3,9 @@
   outputs = { self, artiq }: {
     nixosConfigurations.artiq = artiq.inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit artiq; };
-      modules = [ ./configuration.nix ];
+      # Do not use specialArgs to pass the ARTIQ flake - this evaluates many attributes
+      # and bloats the installation. https://git.m-labs.hk/M-Labs/defenestrate/issues/1
+      modules = [ ( import ./configuration.nix { inherit artiq; } ) ];
     };
   };
 }
